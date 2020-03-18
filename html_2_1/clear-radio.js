6aa24a0be14bd01a6a78258e4f435e08cd18fd1b
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-03-18 16:59:45 +0300

    fix: implement clear-radio-button without js

--- html_2_1/clear-radio.js
+++
@@ -1,9 +0,0 @@
-const ClearRadioButton = document.querySelectorAll(".radio-button__clear-button");
-
-ClearRadioButton.forEach((button) => {
-  button.onclick = (event) => {
-    const targetCheckBoxId = event.target.getAttribute("data-for");
-    const checkBoxToDisable = document.getElementById(targetCheckBoxId);
-    checkBoxToDisable.checked = false;
-  }
-});
